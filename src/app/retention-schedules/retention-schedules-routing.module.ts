ab638ff1b8578efe4af570446fba7cd69d428731
pedro-chavezcastro <pedro.chavezcastro@outlook.com>
2024-10-17 14:47:43 -0500

    main component added

--- src/app/retention-schedules/retention-schedules-routing.module.ts
+++ src/app/retention-schedules/retention-schedules-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./protected/protected.module').then(m => m.ProtectedModule),
+    component: MainComponent,
   }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class RetentionSchedulesRoutingModule { }
+export class RetentionSchedulesRoutingModule {}
