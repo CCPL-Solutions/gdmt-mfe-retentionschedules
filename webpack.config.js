e57425c1a773cefae3078c2f113f2e6b54d4188d
pedro-chavezcastro <pedro.chavezcastro@outlook.com>
2024-10-18 21:54:44 -0500

    port was changed to 4200

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,6 @@ const RetentionSchedulesModuleFederationConfigPlugin = withModuleFederationPlugi
 
 });
 
-RetentionSchedulesModuleFederationConfigPlugin.output.publicPath = 'http://localhost:4201/'
+RetentionSchedulesModuleFederationConfigPlugin.output.publicPath = 'http://localhost:4200/'
 module.exports = RetentionSchedulesModuleFederationConfigPlugin;
 
