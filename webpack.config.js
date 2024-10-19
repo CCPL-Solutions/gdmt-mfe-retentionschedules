642ec570d3e029ad2a9a1aabe1cc3e5da93fb052
pedro-chavezcastro <pedro.chavezcastro@outlook.com>
2024-10-19 11:51:31 -0500

    port 4201

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,6 @@ const RetentionSchedulesModuleFederationConfigPlugin = withModuleFederationPlugi
 
 });
 
-RetentionSchedulesModuleFederationConfigPlugin.output.publicPath = 'http://localhost:4200/'
+RetentionSchedulesModuleFederationConfigPlugin.output.publicPath = 'http://localhost:4201/'
 module.exports = RetentionSchedulesModuleFederationConfigPlugin;
 
